aaa20bfd91f62908a2f819a6b2d1c41521a17aaa
nizhison <ed201112@gmail.com>
2017-11-14 15:00:30 -0500

    q1

--- q1.sql
+++ q1.sql
@@ -19,7 +19,13 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
 
+create TEMP VIEW elections19962016 as 
+	select extract(year from e_date) as year, country_id, party_id, votes/votes_valid::float * 100 as votes
+	from election, election_result
+	where election_result.election_id = election.id;	
+
+
 
 -- the answer to the query 
-insert into q1 
+insert into q1 select  
 
