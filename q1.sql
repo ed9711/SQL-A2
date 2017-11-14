427715d11bc5f355b184897dbd3295ad8b2d0bbd
nizhison <ed201112@gmail.com>
2017-11-14 18:08:21 -0500

    1fin

--- q1.sql
+++ q1.sql
@@ -15,17 +15,41 @@ partyName VARCHAR(100)
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS elections19962016 CASCADE;
 
 -- Define views for your intermediate steps here.
 
 create TEMP VIEW elections19962016 as 
 	select extract(year from e_date) as year, country_id, party_id, votes/votes_valid::float * 100 as votes
 	from election, election_result
-	where election_result.election_id = election.id;	
+	where election_result.election_id = election.id and 1996 <= extract(year from e_date) and extract(year from e_date) <= 		2016;	
+
+DROP VIEW IF EXISTS with_names CASCADE;
+
+create TEMP VIEW with_names as 
+	select year, name as countryName, votes, name_short as partyName
+	from elections19962016, party, country
+	where party.id = party_id and country.id = country_id;
+
+DROP table IF EXISTS range CASCADE;
+
+create table range (
+lowerb INT,
+upperb INT,
+bound VARCHAR(10)
+);
+insert into range values
+(0, 5, '(0-5]'),
+(5, 10, '(5-10]'),
+(10, 20, '(10-20]'),
+(20, 30, '(20-30]'),
+(30, 40, '(30-40]'),
+(40, 100, '(40-]');
 
 
-
+	
 -- the answer to the query 
-insert into q1 select  
+insert into q1 select year, countryName, bound as voteRange, partyName
+		from with_names, range
+		where votes > lowerb and votes <= upperb; 
 
--- q2.sql
+++ q2.sql
@@ -17,9 +17,36 @@ mostRecentlyWonElectionYear INT
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+
+drop view if exists not_winning cascade;
+create temp view not_winning as 
+	select distinct e1.id, e1.election_id, e1.party_id, e1.alliance_id, e1.seats, e1.votes, e1.description 
+	from election_result e1, election_result e2 
+	where e1.election_id = e2.election_id and e1.votes < e2.votes;
+
+
+drop view if exists winning cascade;
+create temp view winning as
+	select election_result.party_id, election_result.election_id
+	from election_result LEFT JOIN not_winning on election_result.id = not_winning.id
+	where not_winning.id is null and election_result.votes is not null;
+
+drop view if exists party_count cascade;
+create temp view party_count as
+	select party_id, count(election_id) as wins
+	from winning
+	group by party_id;
+
+DROP VIEW IF EXISTS avg_win CASCADE;
 
 -- Define views for your intermediate steps here.
+
+create temp view avg_win as 
+	select country.name as countryName, party.name as partyName, fmaily as partyFamily
+	from country, party, party_family, party_count
+	where party.country_id = country.id and party.id = party_family.party_id and party.id = party_count.party_id;
+
+	
 
 
 -- the answer to the query 
