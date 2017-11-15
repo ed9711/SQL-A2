13e4ccc371baa82b471e709ecf7881497229c270
Eddie <ed201112@gmail.com>
2017-11-15 13:24:20 -0500

    3,4

--- q3.sql
+++ q3.sql
@@ -23,12 +23,29 @@ create temp view ratio_election as
 	where electorate is not null and votes_cast is not null;
 
 drop view if exists same_year cascade;
-create temp vie same_year as
-	select r1.country_id, avg(r1.p_ratio), extract(year from r1.e_date)
+create temp view same_year as
+	select r1.country_id, avg(r1.p_ratio) as p_ratio, extract(year from r1.e_date)
 	from ratio_election r1
 	group by r1.country_id, extract(year from r1.e_date);
 
+drop view if exists 2001to2016 cascade;
+create temp view 2001to2016 as
+	select country_id, p_ratio, year
+	from same_year
+	where year <= 2016 and year >= 2001;
+	
+drop view if exists decreasing cascade;
+create temp view decreasing as
+	select distinct t1.country_id
+	from 2001to2016 t1, 2001to2016 t2
+	where t1.country_id = t2.country_id and t1.year < t2.year and t1.p_ratio > t2.p_ratio;
 
 -- the answer to the query 
 insert into q3 
+	select country.name as countryName, 2001to2016.year as year, 2001to2016.p_ratio as participationRatio
+	from 2001to2016, country
+	where not exists (select 1 
+						from decreasing
+						where decreasing.country_id = 2001to2016.country_id)
+			and 2001to2016.country_id = country.id;
 
--- q4.sql
+++ q4.sql
@@ -17,11 +17,50 @@ CREATE TABLE q4(
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS r0_2 CASCADE;
 
 -- Define views for your intermediate steps here.
+create temp view r0_2 as
+	select country_id, count(left_right) as r0_2
+	from party, party_position
+	where party.id = party_position.party_id and left_right > 0 and left_right <= 2
+	group by country_id;
 
-
+	
+drop view if exists r2_4 cascade;
+create temp view r2_4 as
+	select country_id, count(left_right) as r2_4
+	from party, party_position
+	where party.id = party_position.party_id and left_right > 2 and left_right <= 4
+	group by country_id;
+	
+	
+drop view if exists r4_6 cascade;
+create temp view r4_6 as
+	select country_id, count(left_right) as r4_6
+	from party, party_position
+	where party.id = party_position.party_id and left_right > 4 and left_right <= 6
+	group by country_id;
+	
+	
+drop view if exists r6_8 cascade;
+create temp view r6_8 as
+	select country_id, count(left_right) as r6_8
+	from party, party_position
+	where party.id = party_position.party_id and left_right > 6 and left_right <= 8
+	group by country_id;
+	
+	
+drop view if exists r8_10 cascade;
+create temp view r8_10 as
+	select country_id, count(left_right) as r8_10
+	from party, party_position
+	where party.id = party_position.party_id and left_right > 8 and left_right <= 10
+	group by country_id;
 -- the answer to the query 
 INSERT INTO q4 
+	select country.name as countryName, r0_2, r2_4, r4_6, r6_8, r8_10
+	from country, r0_2, r2_4, r4_6, r6_8, r8_10
+	where country.id = r0_2.country_id and country.id = r2_4.country_id and country.id = r4_6.country_id 
+		and country.id = r6_8.country_id and country.id = r8_10.country_id;
 
