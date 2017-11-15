a74b475003d5df94857f9aa0e7e84f805c0f8def
Eddie <ed201112@gmail.com>
2017-11-15 03:55:04 -0500

    3

--- q3.sql
+++ q3.sql
@@ -13,9 +13,20 @@ create table q3(
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS ratio_election CASCADE;
 
 -- Define views for your intermediate steps here.
+
+create temp view ratio_election as 
+	select id, country_id, e_date, votes_cast/electorate::float  as p_ratio
+	from election
+	where electorate is not null and votes_cast is not null;
+
+drop view if exists same_year cascade;
+create temp vie same_year as
+	select r1.country_id, avg(r1.p_ratio), extract(year from r1.e_date)
+	from ratio_election r1
+	group by r1.country_id, extract(year from r1.e_date);
 
 
 -- the answer to the query 
