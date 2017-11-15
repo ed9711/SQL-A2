99b8781dddd0140e30b1a4609f53c8613e69321c
Eddie <ed201112@gmail.com>
2017-11-15 03:07:25 -0500

    q1

--- q2.sql
+++ q2.sql
@@ -31,38 +31,54 @@ create temp view winning as
 	from election_result LEFT JOIN not_winning on election_result.id = not_winning.id
 	where not_winning.id is null and election_result.votes is not null;
 
-drop view if exists party_count cascade;
-create temp view party_count as
-	select party_count.party_id, count(election_id) as wins, county_id
+drop view if exists win_count cascade;
+create temp view win_count as
+	select winning.party_id, count(election_id) as wins, avg(party.country_id) as country_id
 	from winning, party
-	where party.id = party_count.party_id
-	group by party_count.party_id;
+	where party.id = winning.party_id
+	group by winning.party_id;
 
 DROP VIEW IF EXISTS avg_win CASCADE;
 
 -- Define views for your intermediate steps here.
 
 create temp view avg_win as 
-	select country.id as country_id, avg(wins) as avg_wins
-	from country, party_count
-	where party_count.country_id = country.id
+	select country.id as country_id, sum(wins)/avg(party_count.count) as avg_wins
+	from country, win_count, (select country_id, count(id) 
+								from party 
+								group by country_id) as party_count
+	where win_count.country_id = country.id and party_count.country_id = win_count.country_id
 	group by country.id;
 
 drop view if exists answer cascade;
 create temp view answer as
-	select party_count.country_id, party_count.party_id, party_count.wins
-	from party_count, avg_win
-	where party_count.country_id = avg_win.country_id and party_count.win > 3*avg_wins;
+	select win_count.country_id, win_count.party_id, win_count.wins
+	from win_count, avg_win
+	where win_count.country_id = avg_win.country_id and win_count.wins > 3*avg_wins;
 	
 drop view if exists recentWon cascade;
 create temp view recentWon as
-	select 
-	from party_count, winning
+	select answer.party_id, elec.dates as dates, elec.id as election_id
+	from answer, winning, (election join (select answer.party_id, max(e_date) as dates, avg(election.country_id) as c_id
+										from answer, winning, election
+										where answer.party_id = winning.party_id and winning.election_id = election.id
+										group by answer.party_id) as max_date on election.e_date = max_date.dates 
+										and c_id = country_id) as elec
+	where answer.party_id = winning.party_id and winning.election_id = elec.id;
 
+drop view if exists combine cascade;
+create temp view combine as 
+	select country_id, answer.party_id as party_id1, wins, dates, election_id
+	from answer, recentWon
+	where answer.party_id = recentWon.party_id;
+	
 
 -- the answer to the query 
 insert into q2 
-	select countryName, partyName, partyFamily, wonElections, mostRecentlyWonElectionId, mostRecentlyWonElectionYear
-	from country, party, answer, party_family, election
+	select distinct country.name as countryName, party.name as partyName, pt.family as partyFamily,
+		pt.wins as wonElections, pt.election_id as mostRecentlyWonElectionId, 
+		extract(year from pt.date) as mostRecentlyWonElectionYear
+	from country, (party_family right join combine on party_family.party_id = combine.party_id1) as pt, party
+	where pt.country_id = country.id and party.id = pt.party_id1;
 
 
